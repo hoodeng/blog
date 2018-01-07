3a55e7f266e8c33703222cbdfd6c8ac6c9f8b536
unknown <376811626@qq.com>
2018-01-07 23:56:29 +0800

    blog class

---
+++ src/java/com/wuteng/blog/db/Blog.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.wuteng.blog.db;
+
+/**
+ *
+ * @author wu
+ */
+public class Blog {
+
+    public String title;
+    public String content;
+
+    public Blog(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
